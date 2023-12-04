b8a93b5e25669b76cb37103cfb12e651bd93ca10
Tealball <maxvisser8@hotmail.com>
2023-12-04 09:05:32 +0100

    Create process-form.php

---
+++ Camping Project/process-form.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "groeneweide";
+$username = "max";
+$password = "Max=12345;
+$dbname = "database_voor_namen";
+
+// Connectie creëren
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Connectie checken
+if ($conn->connect_error) {
+    die("Connectie mislukt: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $voornaam = $_POST["voornaam"];
+    $achternaam = $_POST["achternaam"];
+
+    // Data in de database zetten
+    $sql = "INSERT INTO people (voornaam, achternaam) VALUES ('$voornaam', '$achternaam')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Nieuwe registratie aangemaakt";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
