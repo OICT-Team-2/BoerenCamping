893c0ba97c2afc074bc8c6b2af2568a94e76db74
Randy <36478478+Erelumnir@users.noreply.github.com>
2023-12-15 18:34:16 +0100

    Index bugfix 2 + Codebase ordening
    - Make the index function according to wireframe
    - Did coderefactoring based on team QC

--- Camping Project/js/script.js
+++ Camping Project/js/script.js
@@ -14,3 +14,17 @@ minus.addEventListener("click", () => {
   getal--
   total.innerHTML = getal
 })
+
+// Makes reservation form stick to the top.
+document.addEventListener("DOMContentLoaded", function () {
+  var bar = document.querySelector(".reservationBar");
+  var barOffsetTop = bar.getBoundingClientRect().top + window.scrollY;
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= barOffsetTop) {
+      bar.classList.add("sticky");
+    } else {
+      bar.classList.remove("sticky");
+    }
+  });
+});
