b2d92684e5ee4bbcb549c6fa3f210ba4cf7a79b7
Siemvdn <150660103+Siemvdn@users.noreply.github.com>
2024-01-10 11:40:10 +0100

    verandering base.html en app.py
    base.html word nu geladen, en een aanpassing in de app.py

--- chatbot 3.0/venv/app.py
+++ chatbot 3.0/venv/app.py
@@ -4,20 +4,25 @@ from chat import get_response
 
 app = Flask(__name__, template_folder='templates')
 
+@app.route("/chatbot", methods=["POST"])
+def chatbot_endpoint():
+    data = request.get_json()
 
-@app.get("/")
-def index_get():
-    print (render_template('base.html'))
-    return render_template("base.html")
+    @app.get("/")
+    def index_get():
+        print (render_template('base.html'))
+        return render_template("base.html")
 
-@app.post("/predict")
-def predict():
-    text = request.get_json().get("message")
-    # TODO: check if text is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    @app.post("/predict")
+    def predict():
+        text = request.get_json().get("message")
+        # TODO: check if text is valid
+        response = get_response(text)
+        message = {"answer": response}
+        return jsonify(message)
 
+    response = {"result": "Chatbot antwoord"}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
