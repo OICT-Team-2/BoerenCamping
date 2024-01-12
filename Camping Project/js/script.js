f6c31d8b8456fb91083ee80a7918c50db9cbfd2f
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-01-12 14:26:15 +0100

    Security Bugfix 1

--- Camping Project/js/script.js
+++ Camping Project/js/script.js
@@ -1,13 +1,17 @@
-// Makes reservation form stick to the top.
 document.addEventListener("DOMContentLoaded", function () {
   var bar = document.querySelector(".reservationBar");
-  var barOffsetTop = bar.getBoundingClientRect().top + window.scrollY;
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= barOffsetTop) {
-      bar.classList.add("sticky");
-    } else {
-      bar.classList.remove("sticky");
-    }
-  });
+  if (bar) {
+    var barOffsetTop = bar.getBoundingClientRect().top + window.scrollY;
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY >= barOffsetTop) {
+        bar.classList.add("sticky");
+      } else {
+        bar.classList.remove("sticky");
+      }
+    });
+  } else {
+    console.error("Element with class 'reservationBar' not found.");
+  }
 });
