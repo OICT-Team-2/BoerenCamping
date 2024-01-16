1026bd92df47c0ad23860d66b53812e76be5de07
rensekin <rens.ege.ekin@gmail.com>
2024-01-16 13:48:17 +0100

    WIP reservation validation

--- Camping Project/js/script.js
+++ Camping Project/js/script.js
@@ -28,3 +28,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// form validation 
+const name = document.getElementById('voornaam')
+const form = document.getElementById('form')
+const errorElement= document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (voornaam.value === '' || voornaam.value == null) {
+    messages.push("Name is required")
+  }
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+
+})
