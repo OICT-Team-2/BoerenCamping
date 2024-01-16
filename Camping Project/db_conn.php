47a272afe35349bacc27bcf6de1f9147fcb0d19b
rensekin <rens.ege.ekin@gmail.com>
2024-01-16 13:48:28 +0100

    made code better

---
+++ Camping Project/db_conn.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "camping_database";
+
+    try {
+    // Connectie creëren
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch(PDOException $e) {
+      die("Connectie mislukt: " . $e->getMessage());
+    }
