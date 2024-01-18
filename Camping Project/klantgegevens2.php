57471f5339bd5694558245e1f50dcf846806c4ce
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-01-18 11:49:05 +0100

    Update Klantgegevens
    - Edit Php
    - Edit css

--- Camping Project/klantgegevens2.php
+++ Camping Project/klantgegevens2.php
@@ -11,35 +11,35 @@
 
       <label for="voornaam1">Voornaam:</label>
       <div id="voornaamError" class="validation-error"></div>
-      <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam" data-isvalid="true">
+      <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam" >
 
       <label for="achternaam">Achternaam:</label>
       <div id="achternaamError" class="validation-error"></div>
-      <input type="text" id="achternaam" name="achternaam" placeholder="Achternaam" data-isvalid="true" >
+      <input type="text" id="achternaam" name="achternaam" placeholder="Achternaam"  >
 
       <label for="plaats">Plaats:</label>
       <div id="plaatsError" class="validation-error"></div>
-      <input type="text" id="plaats" name="plaats" placeholder="Plaats" data-isvalid="true">
+      <input type="text" id="plaats" name="plaats" placeholder="Plaats" >
 
       <label for="postcode">Postcode:</label>
       <div id="postcodeError" class="validation-error"></div>
-      <input type="text" id="postcode" name="postcode" placeholder="Postcode" data-isvalid="true">
+      <input type="text" id="postcode" name="postcode" placeholder="Postcode" >
 
       <label for="straatnaam">Straat:</label>
       <div id="straatnaamError" class="validation-error"></div>
-      <input type="text" id="straatnaam" name="straatnaam" placeholder="Straat" data-isvalid="true">
+      <input type="text" id="straatnaam" name="straatnaam" placeholder="Straat" >
 
       <label for="huisnummer">Huisnummer:</label>
       <div id="huisnummerError" class="validation-error"></div>
-      <input type="text" id="huisnummer" name="huisnummer" placeholder="Huisnummer" data-isvalid="true">
+      <input type="text" id="huisnummer" name="huisnummer" placeholder="Huisnummer" >
 
       <label for="telefoonnummer">Telefoon:</label>
       <div id="telefoonnummerError" class="validation-error"></div>
-      <input type="text" id="telefoonnummer" name="telefoonnummer" placeholder="Telefoon" data-isvalid="true">
+      <input type="text" id="telefoonnummer" name="telefoonnummer" placeholder="Telefoon" >
 
       <label for="email">Email:</label>
       <div id="emailError" class="validation-error"></div>
-      <input type="text" id="email" name="email" placeholder="Email" data-isvalid="true">
+      <input type="text" id="email" name="email" placeholder="Email" >
     </fieldset>
 
     <input type="submit" value="Verzenden">
