18865e1061faa8369e341320ccbd01c3cef45f83
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-01-25 15:38:22 +0100

    Revert "Update klantgegevens2.php"
    This reverts commit c33565cc7762ffd38e1b32c314085b93d319adb2.

--- Camping Project/klantgegevens2.php
+++ Camping Project/klantgegevens2.php
@@ -1,105 +1,51 @@
 <?php include("header.php"); ?>
+
 <header>
-  <h1 class="headtext">Klantgegevens Formulier</h1>
+  <h1 class="headerText">Klantgegevens Formulier</h1>
 </header>
-<br />
+
 <main>
-  <div class="main">
-    <form
-      id="form"
-      action="process-form.php"
-      method="post"
-      onsubmit="return validateForm()"
-    >
-      <fieldset class="fieldset1">
-        <legend class="persoonlijkeinformatie">Persoonlijke Informatie</legend>
+  <form id='form' action="process-form.php" method="post" onsubmit="return validateForm()">
+    <fieldset>
+      <legend>Persoonlijke Informatie</legend>
 
-        <label for="voornaam">Voornaam:</label>
-        <div id="voornaamError" class="validation-error"></div>
-        <input
-          type="text"
-          id="voornaam"
-          name="voornaam"
-          placeholder="Voornaam"
-          required
-        />
+      <label for="voornaam1">Voornaam:</label>
+      <div id="voornaamError" class="validation-error"></div>
+      <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam" >
 
-        <label for="achternaam">Achternaam:</label>
-        <div id="achternaamError" class="validation-error"></div>
-        <input
-          type="text"
-          id="achternaam"
-          name="achternaam"
-          placeholder="Achternaam"
-          required
-        />
-        <label for="telefoonnummer">Telefoon:</label>
-        <div id="telefoonnummerError" class="validation-error"></div>
+      <label for="achternaam">Achternaam:</label>
+      <div id="achternaamError" class="validation-error"></div>
+      <input type="text" id="achternaam" name="achternaam" placeholder="Achternaam"  >
 
-        <input
-          type="tel"
-          id="telefoonnummer"
-          name="telefoonnummer"
-          placeholder="Telefoon"
-          pattern="(\+31|0)([ \-]?\d{1,4}){1,10}"
-          maxlength="14"
-          required
-        />
+      <label for="plaats">Plaats:</label>
+      <div id="plaatsError" class="validation-error"></div>
+      <input type="text" id="plaats" name="plaats" placeholder="Plaats" >
 
-        <label for="email">Email:</label>
-        <div id="emailError" class="validation-error"></div>
+      <label for="postcode">Postcode:</label>
+      <div id="postcodeError" class="validation-error"></div>
+      <input type="text" id="postcode" name="postcode" placeholder="Postcode" >
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          required
-        />
-        <label for="plaats"
-          >Plaats: <span class="optional">(Optioneel)</span></label
-        >
-        <div id="plaatsError" class="validation-error"></div>
-        <input type="text" id="plaats" name="plaats" placeholder="Plaats" />
+      <label for="straatnaam">Straat:</label>
+      <div id="straatnaamError" class="validation-error"></div>
+      <input type="text" id="straatnaam" name="straatnaam" placeholder="Straat" >
 
-        <label for="postcode">
-          Postcode: <span class="optional">(Optioneel)</span></label
-        >
-        <div id="postcodeError" class="validation-error"></div>
-        <input
-          type="text"
-          id="postcode"
-          name="postcode"
-          placeholder="Postcode"
-        />
+      <label for="huisnummer">Huisnummer:</label>
+      <div id="huisnummerError" class="validation-error"></div>
+      <input type="text" id="huisnummer" name="huisnummer" placeholder="Huisnummer" >
 
-        <label for="straatnaam">
-          Straat: <span class="optional">(Optioneel)</span></label
-        >
-        <div id="straatnaamError" class="validation-error"></div>
-        <input
-          type="text"
-          id="straatnaam"
-          name="straatnaam"
-          placeholder="Straat"
-        />
+      <label for="telefoonnummer">Telefoon:</label>
+      <div id="telefoonnummerError" class="validation-error"></div>
+      <input type="text" id="telefoonnummer" name="telefoonnummer" placeholder="Telefoon" >
 
-        <label for="huisnummer"
-          >Huisnummer: <span class="optional">(Optioneel)</span></label
-        >
-        <div id="huisnummerError" class="validation-error"></div>
+      <label for="email">Email:</label>
+      <div id="emailError" class="validation-error"></div>
+      <input type="text" id="email" name="email" placeholder="Email" >
+    </fieldset>
 
-        <input
-          type="text"
-          id="huisnummer"
-          name="huisnummer"
-          placeholder="Huisnummer"
-        />
-      </fieldset>
-      <input type="submit" value="Verzenden" />
-    </form>
-  </div>
+    <input type="submit" value="Verzenden">
+  </form>
 </main>
+
 <?php include("footer.php"); ?>
 
 
